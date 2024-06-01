24444ebe7653a1e3c07ccf8b2fa7ea6a208c10f2
TsuriHefer <tsuri.he@gmail.com>
2024-06-01 19:07:11 +0300

    final desk

--- src/Components/Layout/Footer/Footer.tsx
+++ src/Components/Layout/Footer/Footer.tsx
@@ -1,9 +1,88 @@
+import { useForm } from "react-hook-form";
 import "./Footer.css";
-
+import { faFacebook, faSquareWhatsapp, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 export function Footer(): JSX.Element {
+    const { register, handleSubmit } = useForm();
+
+
     return (
         <div className="Footer">
-			
+            <div className="upper-square"></div>
+            <div className="content">
+
+                <div className="color-overlay"></div>
+
+                <div className="text-container">
+                    <div className="text">
+                        <div className="small-line-deco"></div>
+                        <h2>תנו לנו לנהל לכם את המשרד
+                        </h2>
+                        <p>ב-<span>YUMI</span>, אנו מאמינים שניהול משרדים אפקטיבי משפיע ישירות על פרודוקטיביות ואווירה במקום עבודה. עם רקע עשיר של מעל לעשור בתחום, חברתנו מספקת פתרונות מותאמים לכל סוגי הארגונים והמבנים. מאחזקה שוטפת ועד ניקיון מקצועי, אנו כאן כדי להבטיח שהמשרד שלכם יהיה יעיל, נקי ומזמין.
+                        </p>
+                        <p>
+                            השאירו פרטים ונשמח לעזור לכם בניהול מלא כולל תחזוקה, הסירו דאגה מליבכם ותנו למקצוענים לטפל בכל!</p>
+                        <div className="social-container">
+                            <div className="phone">
+                                <FontAwesomeIcon icon={faPhone} />
+                                <p>0555555555</p>
+                            </div>
+                            <div className="email">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            <p>yomi@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className="contact">
+                    <h2>יצירת קשר</h2>
+                    <p>מכאן מתחילים השאירו פרטים ונחזור בהקדם האפשרי</p>
+                    <form action="">
+                        <label htmlFor="">
+
+                            <input type="text" {...register("first")} placeholder="אימייל" />
+                        </label>
+
+                        <label htmlFor="">
+
+                            <input type="text" {...register("first")} placeholder="שם" className="name-input" />
+                        </label>
+
+                        <label htmlFor="">
+
+                            <input type="text" {...register("first")} placeholder="עיר" />
+                        </label>
+
+                        <label htmlFor="">
+
+                            <input type="text" {...register("first")} placeholder="טלפון" />
+                        </label>
+
+                        <label htmlFor="">
+
+                            <input type="text" {...register("first")} placeholder="מספר  קומות" />
+                        </label>
+
+                        <label htmlFor="">
+
+                            <input{...register("first")} placeholder="מספר דירות בבניין" />
+                        </label>
+
+                        <label htmlFor="" className="message" >
+                            <textarea  {...register("first")} placeholder="הודעה" ></textarea>
+                        </label>
+
+                        <button className="send-btn">שליחה</button>
+                    </form>
+                </div>
+
+            </div>
+
+
         </div>
     );
 }
--- src/Components/Layout/Hero/Hero.tsx
+++ src/Components/Layout/Hero/Hero.tsx
@@ -4,6 +4,14 @@ import buildingHero from "../../../images/buildinghero.png"
 import leftChevron from "../../../images/leftchevron.svg"
 import heroWave from "../../../images/herowave.svg"
 
+import binIcon from "../../../images/bin-icon.svg"
+import maintenancesIcon from "../../../images/maintenence-icon.svg"
+import washingIcon from "../../../images/washing-icon.svg"
+import windowsIcon from "../../../images/windows-icon.svg"
+import safetyIcon from "../../../images/safety-icon.svg"
+import polishIcon from "../../../images/polish-icon.svg"
+import paintingIcon from "../../../images/painting-icon.svg"
+import inventoryIcon from "../../../images/inventory-icon.svg"
 
 export function Hero(): JSX.Element {
     return (
@@ -44,9 +52,75 @@ export function Hero(): JSX.Element {
             </div>
 
             <div className="wave-container">
-                <img src={heroWave} alt="" className="hero-wave"/>
-                <div className="rectangle-wave"></div>
+                <img src={heroWave} alt="" className="hero-wave" />
+                <div className="rectangle-wave">
+                    <div className="services-icons">
+
+                        <div className="icon-container">
+                            <img src={binIcon} alt="" className="maintenances" />
+                            <p>סביבה</p>
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={maintenancesIcon} alt="" className="maintenances" />
+                            <p>תחזוקה</p>
+
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={washingIcon} alt="" className="maintenances" />
+                            <p>שטיפה</p>
+
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={windowsIcon} alt="" className="maintenances" />
+                            <p>חלונות</p>
+
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={safetyIcon} alt="" className="maintenances" />
+                            <p>בטיחות</p>
+
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={paintingIcon} alt="" className="maintenances" />
+                            <p>צביעה</p>
+
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={polishIcon
+                            } alt="" className="maintenances" />
+                            <p>הברקה</p>
+
+                        </div>
+
+                        <div className="icon-container">
+                            <img src={inventoryIcon} alt="" className="maintenances" />
+                            <p>מלאי</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
         </div >
     );
 } 
